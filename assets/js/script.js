41edd98d830753c6dd23bbadf2937e01b30102f7
AM77 <amine.griche77@gmail.com>
2019-05-03 17:14:02 +0200

    finished some style and removed useless files.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,54 @@
 $(window).scroll(function () {
     var header = $('#header'),
         scroll = $(window).scrollTop(),
-        logo = $('#header #logo img')
+        logo = $('#header #logo img'),
+        home_nav = $('#mobile-bar.home'),
+        product_nav = $('#mobile-bar.product'),
+        team_nav = $('#mobile-bar.team')
 
     if (scroll >= 60) {
         header.addClass('fix-header')
-        logo.removeClass('hide')
+        logo.removeClass('hide').addClass('show')
     } else {
-        header.removeClass('fix-header')
-        logo.addClass('hide')
+        if (scroll == 0) {
+            header.removeClass('fix-header')
+            logo.addClass('hide').removeClass('show')
+        }
     }
+
+    if (scroll >= 240) {
+        home_nav.removeClass('close').addClass('open')
+    } else {
+        if (scroll == 0) {
+            home_nav.addClass('close').removeClass('open')
+        }
+    }
+
+    if (scroll >= 180) {
+        product_nav.removeClass('close').addClass('open')
+        team_nav.removeClass('close').addClass('open')
+    } else {
+        if (scroll == 0) {
+            product_nav.addClass('close').removeClass('open')
+            team_nav.addClass('close').removeClass('open')
+        }
+    }
+});
+
+$('.menu-button').click(function(){
+    if($(this).hasClass("close")){
+        $(this).addClass('open').removeClass('close')
+        $('.sidebar').addClass('close').removeClass('open')
+    }else{
+        $(this).addClass('close').removeClass('open')
+        $('.sidebar').addClass('open').removeClass('close')
+    }
+})
+
+$(".button.try-it").click(function() {
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("#our-products").offset().top - 60
+    }, 500);
 });
 
 var carousel = function () {
@@ -25,18 +64,25 @@ var carousel = function () {
             dots: true,
             navText: ['<span class="icon-arrow_back">', '<span class="icon-arrow_forward">'],
             responsive: {
-                600: {
+
+                0:{
                     margin: 20,
                     nav: true,
-                    items: 4
+                    items: 1
                 },
-                1000: {
+
+                400: {
+                    margin: 20,
+                    nav: true,
+                    items: 2
+                },
+                900: {
                     margin: 30,
                     stagePadding: 0,
                     nav: true,
                     items: 4
                 },
-                1200: {
+                1600: {
                     margin: 30,
                     stagePadding: 0,
                     nav: true,
